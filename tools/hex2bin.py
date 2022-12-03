0f5780695b15802c23af33c25fc8142aca4869ca
Paul Brannan <cout@users.noreply.github.com>
2022-12-02 21:20:28 -0500

    Add a tool to convert a room from binary to ascii hex

--- tools/hex2bin.py
+++ tools/hex2bin.py
@@ -3,16 +3,31 @@
 import sys
 import re
 
-def main():
-  s = sys.stdin.read()
-  # s = s.replace(',', ' ')
-  # s = s.replace('|', ' ')
-  # s = s.replace('+', ' ')
-  # s = s.replace('-', ' ')
-  s = re.sub('[^A-Za-z0-9]', ' ', s)
+def strip_comments(s):
+  return re.sub('\s*;.*(?=\n|\r)', '', s, re.MULTILINE)
+
+def replace_non_hex_with_whitespace(s):
+  return re.sub('[^A-Za-z0-9]', ' ', s)
+
+def long_to_little_endian(s):
+  return re.sub('\\b([A-Za-z0-9]{2})([A-Za-z0-9]{2})([A-Za-z0-9]{2})\\b', '\\3 \\2 \\1', s)
+
+def short_to_little_endian(s):
+  return re.sub('\\b([A-Za-z0-9]{2})([A-Za-z0-9]{2})\\b', '\\2 \\1', s)
+
+def hex2bin(s):
+  s = strip_comments(s)
+  s = replace_non_hex_with_whitespace(s)
+  s = long_to_little_endian(s)
+  s = short_to_little_endian(s)
   l = [ int(x, 16) for x in s.split() ]
   b = bytes(l)
-  sys.stdout.buffer.write(bytearray(b))
+  return bytearray(b)
+
+def main():
+  s = sys.stdin.read()
+  b = hex2bin(s)
+  sys.stdout.buffer.write(b)
 
 if __name__ == '__main__':
   main()
---
+++ tools/room2hex.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import sys
+import struct
+import re
+
+final_format = \
+'''
+; Size:
+%s
+
+; Tilemap:
+%s
+
+; BTS:
+%s
+'''
+
+def bin2room(b):
+  size, = struct.unpack('<H', b[0:2])
+  size = size // 2
+  fmt = '%dH%dB' % (size, size)
+  l = struct.unpack(fmt, b[2:])
+  tilemap = l[0:size]
+  bts = l[size:]
+  return size, tilemap, bts
+
+def split_into_rows(l, w, h):
+  return [ l[i:i+w] for i in range(0, h*w, w) ]
+
+def format_row(row, fmt):
+  return ' '.join(fmt % x for x in row)
+
+def format_rows(rows, fmt):
+  return [ format_row(row, fmt) for row in rows ]
+
+def add_seps(rows):
+  srows = [ ]
+
+  for i in range(0, len(rows)):
+    row = rows[i]
+    row = re.sub('(([A-Za-z0-9]+ ){15}[A-Za-z0-9]+) ', '\\1|', row)
+    srows.append(row)
+
+    if i % 16 == 15:
+      sep = re.sub('[A-Za-z0-9 .]', '-', row)
+      sep = re.sub('[|]', '+', sep)
+      srows.append(sep)
+
+  return srows
+
+def format_grid(rows, fmt):
+  rows = format_rows(rows, fmt)
+  rows = add_seps(rows)
+  return '\n'.join(rows)
+
+def format_tilemap(tilemap, w, h):
+  rows = split_into_rows(tilemap, w, h)
+  return format_grid(rows, '%04X')
+
+def format_bts(bts, w, h):
+  rows = split_into_rows(bts, w, h)
+  return format_grid(rows, '%02X')
+
+def room2hex(b, w, h):
+  size, tilemap, bts = bin2room(b)
+  tilemap_bytes = size * 2
+  hex_size = '%02X %02X' % (tilemap_bytes & 0xff, tilemap_bytes >> 8)
+  hex_tilemap = format_tilemap(tilemap, w, h)
+  hex_bts = format_bts(bts, w, h)
+  return (final_format % (hex_size, hex_tilemap, hex_bts)).lstrip()
+
+def main():
+  ws = int(sys.argv[1])
+  hs = int(sys.argv[2])
+  w = ws * 16
+  h = hs * 16
+  b = sys.stdin.buffer.read()
+  s = room2hex(b, w, h)
+  sys.stdout.write(s)
+
+if __name__ == '__main__':
+  main()
