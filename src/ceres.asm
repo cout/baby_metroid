6787546e73f695cf08b3967c4ff55ac5e8aa7537
Paul Brannan <cout@users.noreply.github.com>
2022-12-09 15:24:47 -0500

    Use save station electricity as a transporter effect

--- src/ceres.asm
+++ src/ceres.asm
@@ -1,26 +1,83 @@
 ;;
 ; Increase required health for ridley to fly away
 org $A6A6F4
+
 CMP #$0777
 
 ;;
-; Disable ceres escape timer
-;
-; TODO - now Samus can't get out!
+; Replace ceres escape with transporter
 
-org $A6C0BB
+org $A6C117 ; ceres escape timer has started
 
-;; ; Spawn save station electricity enemy projectile
-;; ; TODO - this does not work
-;; LDX $1C27 ; what is this for?
-;; LDY #$E6D2
-;; JSL $868097 ; room graphics
-;; ; JSL $868097 ; enemy graphics
+JMP state_start_transport_sequence
 
-; Made it to Ceres elevator
-; TODO - I would like this to be "beam out" sequence instead of immediately
-; fading to black
-LDA #$0020
-STA $0998
+org !FREESPACE_A6
 
-RTS
+state_start_transport_sequence:
+{
+  LDA.w #transporter_electricity
+  TAY
+  LDX #$0000
+  JSL $868097
+
+  ; TODO:
+  ; 1. Play a sound for the transport sequence
+  ; 2. Give Samus iframes for effect
+  LDA #$0080
+  STA $0FB0
+
+  LDA.w #state_transporter_operating
+  STA $0FA8 ; next state
+
+  RTS
+}
+
+state_transporter_operating:
+{
+  DEC $0FB0
+  BNE .return
+
+  ; Start the cutscene
+  LDA #$0020
+  STA $0998
+
+.return:
+  ; TODO - I don't know what this call is for, just that the routines
+  ; that I'm replacing invoke it.
+  ; JSR $C19C
+
+  RTS
+}
+
+end_ceres_freespace_a6:
+!FREESPACE_A6 = end_ceres_freespace_a6
+
+org !FREESPACE_86
+
+transporter_electricity:
+
+dw transporter_electricity_init ; init ai
+dw transporter_electricity_main ; pre-instruction
+dw $E683                        ; instruction list
+dw $00                          ; x radius
+dw $00                          ; y radius
+dw $3000                        ; properties
+dw $0000                        ; hit instruction list
+dw $84FC                        ; shot instruction list
+
+transporter_electricity_init:
+{
+  RTS
+}
+
+transporter_electricity_main:
+{
+  ; Move the electricity with Samus
+  LDA $0AF6 : STA $1A4B,x
+  LDA $0AFA : STA $1A93,x
+
+  RTS
+}
+
+ceres_freespace_86_end:
+!FREESPACE_86 := ceres_freespace_86_end
--- src/freespace.asm
+++ src/freespace.asm
@@ -1,3 +1,4 @@
+!FREESPACE_86 = $86F4A6
 !FREESPACE_A0 = $A0F7D3
 !FREESPACE_A1 = $A0F7D3
 !FREESPACE_8F = $8FE99B
@@ -12,8 +13,12 @@
 !FREESPACE_A3 = $A3F311 ; 3311 bytes
 !FREESPACE_A4 = $A3F6C0 ; 2368 bytes
 !FREESPACE_A5 = $A5F95A ; 1702 bytes
-!FREESPACE_A6 = $A5FEBC ; 324 bytes
-!FREESPACE_A7 = $A5FF82 ; 126 bytes
+!FREESPACE_A6 = $A6FEBC ; 324 bytes
+!FREESPACE_A7 = $A7FF82 ; 126 bytes
 !FREESPACE_A8 = $A8F9BE ; 1602 bytes
 !FREESPACE_A9 = $A8FB70 ; 1168 bytes
 !FREESPACE_AA = $A8F7D3 ; 2093 bytes
+
+; TODO TODO TODO - above values are unaligned addresses; could this be
+; the cause of mysterious crashes when I try to put an enemy (samus
+; statue) in banks A2, A4, or AA?
