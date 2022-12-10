e5aeadbad50eda1b548fa90efe15313547e144c3
Paul Brannan <cout@users.noreply.github.com>
2022-12-09 21:26:37 -0500

    Disable Kraid walking and lunging

--- src/bosses/kraid.asm
+++ src/bosses/kraid.asm
@@ -1,5 +1,6 @@
 ;;
 ; Disable Kraid spitting rocks
+;
 
 org $A7BC14 ; jsr for spawn rock projectile
 
@@ -8,3 +9,19 @@ BRA .earthquake_sound
 org $A7BC1B
 
 .earthquake_sound:
+
+;;
+; Disable lunging
+;
+
+org $A786F3 ; walk forward
+
+dw $B633, $812F
+
+org $A787BD ; lunge forward
+
+dw $B633, $812F
+
+org $A78887 ; walk backward
+
+dw $B633, $812F
