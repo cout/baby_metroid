d8606eca07575baf0ebe5e68b9f2804c061f4381
Paul Brannan <cout@users.noreply.github.com>
2022-12-13 23:07:15 -0500

    Include layer2 tilemap in hex dump

--- tools/room2hex.py
+++ tools/room2hex.py
@@ -18,14 +18,30 @@ final_format = \
 {bts}
 '''
 
+layer2_format = \
+'''
+; Layer 2 Tilemap:
+{layer2_tilemap}
+'''
+
 def bin2room(b):
   size, = struct.unpack('<H', b[0:2])
   size = size // 2
-  fmt = '%dH%dB' % (size, size)
-  l = struct.unpack(fmt, b[2:])
-  tilemap = l[0:size]
-  bts = l[size:]
-  return size, tilemap, bts
+  if len(b) == 2 + size * 3:
+    fmt = '%dH%dB' % (size, size)
+    l = struct.unpack(fmt, b[2:])
+    tilemap = l[0:size]
+    bts = l[size:]
+    return size, tilemap, bts, None
+  elif len(b) == 2 + size * 5:
+    fmt = '%dH%dB%dH' % (size, size, size)
+    l = struct.unpack(fmt, b[2:])
+    tilemap = l[0:size]
+    bts = l[size:size*2]
+    layer2_tilemap = l[size*2:]
+    return size, tilemap, bts, layer2_tilemap
+  else:
+    raise RuntimeError("Size of data (%s) did not match level data size (%s) -- expected either %s or %s" % (len(b), size, size*3+2, size*5+2)) # TODO
 
 def split_into_rows(l, w, h):
   return [ l[i:i+w] for i in range(0, h*w, w) ]
@@ -65,12 +81,12 @@ def format_bts(bts, w, h):
   return format_grid(rows, ' %02X ')
 
 def room2hex(b, w, h):
-  size, tilemap, bts = bin2room(b)
+  size, tilemap, bts, layer2_tilemap = bin2room(b)
   tilemap_bytes = size * 2
   hex_size = '%02X %02X' % (tilemap_bytes & 0xff, tilemap_bytes >> 8)
   hex_tilemap = format_tilemap(tilemap, w, h)
   hex_bts = format_bts(bts, w, h)
-  return final_format.format(
+  s = final_format.format(
       width_s=w//16,
       height_s=h//16,
       width_t=w,
@@ -80,6 +96,10 @@ def room2hex(b, w, h):
       tilemap=hex_tilemap,
       bts=hex_bts,
   ).lstrip()
+  if layer2_tilemap:
+    hex_layer2_tilemap = format_tilemap(layer2_tilemap, w, h)
+    s += layer2_format.format(layer2_tilemap=hex_layer2_tilemap)
+  return s
 
 def main():
   ws = int(sys.argv[1])
