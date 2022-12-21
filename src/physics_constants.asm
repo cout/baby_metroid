05446fa83fc736f1759c21291fe376491d99b03a
Paul Brannan <cout@users.noreply.github.com>
2022-12-20 22:58:43 -0500

    Adjust physics constants for space jump

--- src/physics_constants.asm
+++ src/physics_constants.asm
@@ -4,3 +4,9 @@ dw $0000 ; lava subdamage per frame
 dw $0000 ; lava damage per frame
 dw $0000 ; acid subdamage per frame
 dw $0000 ; acid damage per frame
+dw $0003 ; animation delay in water
+dw $0002 ; animation delay in lava/acid
+dw $0000 ; space jump min Y velocity in air
+dw $7FFF ; space jump max Y velocity in air
+dw $0000 ; space jump min Y velocity in liquid
+dw $7FFF ; space jump max Y velocity in liquid
