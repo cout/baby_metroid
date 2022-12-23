44ccb28e9d4b41bc687b7af8f1cf76ac00b957dc
Paul Brannan <cout@users.noreply.github.com>
2022-12-23 07:30:58 -0500

    Disable escape timer

--- src/enemies/mother_brain.asm
+++ src/enemies/mother_brain.asm
@@ -46,3 +46,38 @@ JMP $AF91
 
 org $A9B127
 JMP $B1BE
+
+;;
+; Disable time bomb text
+;
+
+org $A9B21C
+LDA #$B2F9 ; TODO - this skips "Load exploded escape door particles tiles"
+
+;;
+; Disable escape timer
+;
+
+org $A9B302
+JMP $B30F
+
+;;
+; Disable escape explosions
+;
+
+org $A9B313
+JMP $B31A
+
+;;
+; Disable earthquake
+;
+
+org $A9B33C
+
+LDA #$0000
+STA $1840
+RTS
+
+; TODO - diagnose baby graphical glitch by pointing various state
+; changes here to find out which one causes the glitch
+mother_brain_null_state = $A9B345
