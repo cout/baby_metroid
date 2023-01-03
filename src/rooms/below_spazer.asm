bf2074630103b5fb48327faf1870cc9a4d30fb45
Paul Brannan <cout@users.noreply.github.com>
2023-01-02 21:03:02 -0500

    Remove yapping maws from below spazer room

--- src/rooms/below_spazer.asm
+++ src/rooms/below_spazer.asm
@@ -1,68 +1,52 @@
-; TODO TODO TODO -
-; Below code freezes the enemies in the room, but this code crashes
-; sometimes, so there needs to be an easier way through the room
+; Room $A408: Header
+org $8FA408
+db $28       ; Index
+db $01       ; Area
+db $24       ; X position on map
+db $11       ; Y position on map
+db $02       ; Width (in screens)
+db $02       ; Height (in screens)
+db $70       ; Up scroller
+db $A0       ; Down scroller
+db $00       ; Special graphics bits
+dw $A42F     ; Door out
+dw $E5E6     ; State $A415 function (default)
 
-;;;   !room_initialized = $7FFB02
-;;;   
-;;;   ;;
-;;;   ; Set main room handler
-;;;   
-;;;   org $8FA427
-;;;   
-;;;   dw below_spazer_main_handler
-;;;   
-;;;   org $8FA42D
-;;;   
-;;;   dw below_spazer_init
-;;;   
-;;;   ;;
-;;;   ; Keep all the enemies in the room frozen
-;;;   
-;;;   org !FREESPACE_8F
-;;;   
-;;;   copy_ice_palette:
-;;;   {
-;;;     LDA $90C401 : STA $7EC120 : STA $7EC140
-;;;     LDA $90C403 : STA $7EC122 : STA $7EC142
-;;;     LDA $90C405 : STA $7EC124 : STA $7EC144
-;;;     LDA $90C407 : STA $7EC126 : STA $7EC146
-;;;     LDA $90C409 : STA $7EC128 : STA $7EC148
-;;;     LDA $90C40B : STA $7EC12A : STA $7EC14A
-;;;     LDA $90C40D : STA $7EC12C : STA $7EC14C
-;;;     LDA $90C40F : STA $7EC12E : STA $7EC14E
-;;;     LDA $90C411 : STA $7EC130 : STA $7EC150
-;;;     LDA $90C413 : STA $7EC132 : STA $7EC152
-;;;     LDA $90C415 : STA $7EC134 : STA $7EC154
-;;;     LDA $90C417 : STA $7EC136 : STA $7EC156
-;;;     LDA $90C419 : STA $7EC138 : STA $7EC158
-;;;     LDA $90C41B : STA $7EC13A : STA $7EC15A
-;;;     LDA $90C41D : STA $7EC13C : STA $7EC15C
-;;;     LDA $90C41F : STA $7EC13E : STA $7EC15E
-;;;   
-;;;     RTS
-;;;   }
-;;;   
-;;;   below_spazer_main_handler:
-;;;   {
-;;;     LDA !room_initialized
-;;;     BEQ .return
-;;;   
-;;;     LDA #$0001
-;;;     STA $185E ; set "enemy time is frozen" flag
-;;;   
-;;;     JSR copy_ice_palette
-;;;   
-;;;     LDA #$0000
-;;;     STA !room_initialized
-;;;   
-;;;   .return
-;;;     RTS
-;;;   }
-;;;   
-;;;   below_spazer_init:
-;;;   {
-;;;     LDA #$0001
-;;;     STA !room_initialized
-;;;   
-;;;     RTS
-;;;   }
+; Room $A408 state $A415: Header
+; (default)
+org $8FA415
+dl $C6B91C   ; Level data address
+db $07       ; Tileset
+db $00       ; Music data index
+db $05       ; Music track index
+dw $83C0     ; FX address (bank $83)
+dw $95E4     ; Enemy population offset (bank $A1)
+dw $8449     ; Enemy graphics set offset (bank $B4)
+dw $C1C1     ; Layer 2 scroll
+dw $A435     ; Room scroll data (bank $8F)
+dw $0000     ; Room var
+dw $0000     ; Room main routine (bank $8F)
+dw $891E     ; Room PLM list address (bank $8F)
+dw $BC02     ; Library background (bank $8F)
+dw $91D6     ; Room setup routine (bank $8F)
+
+; Room $A408 state $A415: Enemy population
+org $A195E4
+;  enemy  x      y      init   props  extra  param1 param2
+; dw $E7BF, $0190, $01C8, $0000, $2000, $0000, $0030, $0001 ; yapping maw
+; dw $E7BF, $0080, $01C8, $0000, $2000, $0000, $0030, $0001 ; yapping maw
+dw $CFFF, $0108, $0193, $0000, $2000, $0000, $0100, $0200 ; cacatac
+dw $FFFF     ; end of list
+db $01       ; death quota
+
+; Room $A408 state $A415: Enemy graphics set
+org $B48449
+;  enemy  palette
+; dw $E7BF, $0001 ; yapping maw
+dw $CFFF, $0002 ; cacatac
+dw $FFFF     ; end of list
+
+; Room $A408 state $A415: FX
+org $8383C0
+;  door   base   target veloc     time  type  A    B    C   pal  anim blend
+dw $0000, $01BE, $FFFF, $0000 : db $00, $06, $02, $18, $03, $00, $01, $48
