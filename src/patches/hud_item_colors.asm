c86d4b729adc3b4e427e1e0c14847f2b13849b67
Paul Brannan <cout@users.noreply.github.com>
2023-01-29 15:09:41 -0500

    Eliminate flicker on mesen-S

--- src/patches/hud_item_colors.asm
+++ src/patches/hud_item_colors.asm
@@ -1,9 +1,10 @@
 !begin_hud_colors_irq_command = #begin_hud_colors_entry-$9616
-!end_hud_colors_irq_command = #end_hud_colors_entry-$9616
+!end_hud_colors_0_irq_command = #end_hud_colors_0_entry-$9616
+!end_hud_colors_1_irq_command = #end_hud_colors_1_entry-$9616
 
-org $80969F : LDA.w !end_hud_colors_irq_command
-org $8096A2 : LDY #$001E
-org $8096A5 : LDX #$0098
+org $80969F : LDA.w !end_hud_colors_0_irq_command
+org $8096A2 : LDY #$001D
+org $8096A5 : LDX #$0090
 
 org $8096C9 : LDA.w !begin_hud_colors_irq_command
 org $8096CC : LDY #$00F5
@@ -21,23 +22,13 @@ JSR store_hud_item_index
 org !FREESPACE_80
 
 extra_irq_commands:
-begin_hud_colors_entry: dw begin_hud_drawing_hook
-end_hud_colors_entry:   dw end_hud_drawing_hook
+begin_hud_colors_entry: dw begin_hud_colors_irq_command_handler
+end_hud_colors_0_entry: dw end_hud_colors_0_irq_command_handler
+end_hud_colors_1_entry: dw end_hud_colors_1_irq_command_handler
 
-begin_hud_drawing_hook:
+begin_hud_colors_irq_command_handler:
 {
-  SEP #$30
-  LDX !hud_selected_item_palette_index
-  ; LDA #$8F : STA $2100
-  LDA #$11 : STA $2121
-  LDA hud_selected_item_palette+$02,x : STA $2122
-  LDA hud_selected_item_palette+$03,x : STA $2122
-  LDA hud_selected_item_palette+$04,x : STA $2122
-  LDA hud_selected_item_palette+$05,x : STA $2122
-  LDA hud_selected_item_palette+$06,x : STA $2122
-  LDA hud_selected_item_palette+$07,x : STA $2122
-  ; LDA $51 : STA $2100
-  REP #$30
+  JSR set_hud_colors
 
   LDA #$0004
   LDY #$0000
@@ -45,10 +36,41 @@ begin_hud_drawing_hook:
   RTS
 }
 
-end_hud_drawing_hook:
+end_hud_colors_0_irq_command_handler:
+{
+  JSR restore_green_door_colors_0
+
+  LDA !end_hud_colors_1_irq_command
+  LDY #$001E
+  LDX #$0090
+  RTS
+}
+
+
+end_hud_colors_1_irq_command_handler:
+{
+  JSR restore_green_door_colors_1
+
+  LDA #$0006
+  LDY #$001F
+  LDX #$0098
+  RTS
+}
+
+hud_selected_item_palette:
+{
+  dw $0000, $0BB1, $1EA9, $0145 ; nothing (and green doors)
+  dw $0000, $72BC, $48FB, $1816 ; missiles
+  dw $0000, $0BB1, $1EA9, $0145 ; super missiles
+  dw $0000, $02DF, $01D7, $00AC ; power bombs
+  dw $0000, $72B2, $71C7, $4D03 ; grapple beam
+  dw $0000, $72B2, $71C7, $4D03 ; xray
+}
+
+set_hud_colors:
 {
   SEP #$30
-  LDX #$00
+  LDX !hud_selected_item_palette_index
   LDA #$8F : STA $2100
   LDA #$11 : STA $2121
   LDA hud_selected_item_palette+$02,x : STA $2122
@@ -59,22 +81,28 @@ end_hud_drawing_hook:
   LDA hud_selected_item_palette+$07,x : STA $2122
   LDA $51 : STA $2100
   REP #$30
-
-  LDA #$0006
-  LDY #$001F
-  LDX #$0098
   RTS
 }
 
-hud_selected_item_palette:
-print "hud_selected_item_palette: ", pc
+restore_green_door_colors_0:
 {
-  dw $0000, $0BB1, $1EA9, $0145 ; nothing (and green doors)
-  dw $0000, $72BC, $48FB, $1816 ; missiles
-  dw $0000, $0BB1, $1EA9, $0145 ; super missiles
-  dw $0000, $02DF, $01D7, $00AC ; power bombs
-  dw $0000, $72B2, $71C7, $4D03 ; grapple beam
-  dw $0000, $72B2, $71C7, $4D03 ; xray
+  SEP #$30
+  LDA #$11 : STA $2121
+  LDA hud_selected_item_palette+$02 : STA $2122
+  LDA hud_selected_item_palette+$03 : STA $2122
+  REP #$30
+  RTS
+}
+
+restore_green_door_colors_1:
+{
+  SEP #$30
+  LDA hud_selected_item_palette+$04 : STA $2122
+  LDA hud_selected_item_palette+$05 : STA $2122
+  LDA hud_selected_item_palette+$06 : STA $2122
+  LDA hud_selected_item_palette+$07 : STA $2122
+  REP #$30
+  RTS
 }
 
 store_hud_item_index:
