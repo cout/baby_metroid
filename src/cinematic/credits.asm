a2e496654c6589b21640b54e2d741476f7dce1d9
Paul Brannan <cout@users.noreply.github.com>
2023-02-02 11:46:10 -0500

    Add credits

--- src/cinematic/credits.asm
+++ src/cinematic/credits.asm
@@ -53,43 +53,84 @@ org $8CDB4B-$02 : dw !credits_separation_lines
 org $8CDBA3-$02 : dw !credits_separation_lines
 org $8CDC73-$02 : dw !credits_separation_lines
 
+!extra_credit_lines := ((16-!credits_separation_lines)*20)
+print "Extra credit lines available: ", dec(!extra_credit_lines)
+
 org $8CDC8F-$04 : dw I_goto_long : dl baby_metroid_credits
 
 org !FREESPACE_B8
 
 baby_metroid_credits:
 {
-  dw I_set_timer, !credits_separation_lines
-  dw I_set_timer, !credits_separation_lines
+  dw I_set_timer, $000F
 - dw I_draw, blank_line
   dw I_loop, -
 
-  !small(white)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 0
-  !small(yellow)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 1
-  !small(cyan)    : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 2
-  !small(green)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 3
-  !small(pink)    : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 4
-  !small(blue)    : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 5
-  !small(orange)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 6
-  !small(purple)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,':! " ; 7
-  !large1(white)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 8
-  !large2(white)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 9
-  !large1(yellow) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 10
-  !large2(yellow) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 11
-  !large1(cyan)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 12
-  !large2(cyan)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 13
-  !large1(green)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 14
-  !large2(green)  : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 15
-  !large1(pink)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 16
-  !large2(pink)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 16
-  !large1(blue)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 18
-  !large2(blue)   : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 19
-  !large1(orange) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 20
-  !large2(orange) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 21
-  !large1(purple) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ  '""  " ; 22
-  !large2(purple) : dw I_write, "ABCDEFGHIJKLMNOPQRSTUVWXYZ.,    "  ; 23
-  !large1(purple) : dw I_write, "0123456789                      "  ; 24
-  !large2(purple) : dw I_write, "0123456789                      "  ; 25
+.extra_credits_start:
+!small(green)   : dw I_write, " THANKS TO EVERYONE IN METCONST "
+!small(green)   : dw I_write, "   WHO HELPED MAKE THIS DREAM   "
+!small(green)   : dw I_write, "        INTO A REALITY          "
+!small(green)   : dw I_write, "                                "
+!large1(white)  : dw I_write, " TESTRUNNER PJBOY INSANEFIREBAT "
+!large2(white)  : dw I_write, " TESTRUNNER PJBOY INSANEFIREBAT "
+!large1(white)  : dw I_write, "  OI27 MOEHR CRAZY NEEN AMOEBA  "
+!large2(white)  : dw I_write, "  OI27 MOEHR CRAZY NEEN AMOEBA  "
+!large1(white)  : dw I_write, "  SMILEY OUICHEGEANTE NODEVER2  "
+!large2(white)  : dw I_write, "  SMILEY OUICHEGEANTE NODEVER2  "
+!large1(white)  : dw I_write, "    METROIDNERD9001 SOMERANDO   "
+!large2(white)  : dw I_write, "    METROIDNERD9001 SOMERANDO   "
+!large1(white)  : dw I_write, "  OB NOBODYNADA STROTLOG YURIK  "
+!large2(white)  : dw I_write, "  OB NOBODYNADA STROTLOG YURIK  "
+!large1(white)  : dw I_write, "     SCYZER EXISTER BENOX50     "
+!large2(white)  : dw I_write, "     SCYZER EXISTER BENOX50     "
+!large1(white)  : dw I_write, "        AND MANY OTHERS         "
+!large2(white)  : dw I_write, "        AND MANY OTHERS         "
+!small(white)   : dw I_write, "                                " ; 1
+!small(white)   : dw I_write, "                                " ; 2
+!small(white)   : dw I_write, "                                " ; 3
+!small(white)   : dw I_write, "                                " ; 4
+!small(white)   : dw I_write, "                                " ; 5
+!small(white)   : dw I_write, "                                " ; 6
+!small(white)   : dw I_write, "                                " ; 7
+!small(white)   : dw I_write, "                                " ; 8
+!small(white)   : dw I_write, "                                " ; 9
+!small(white)   : dw I_write, "                                " ; 10
+!small(white)   : dw I_write, "                                " ; 11
+!small(white)   : dw I_write, "                                " ; 12
+!small(white)   : dw I_write, "                                " ; 13
+!small(white)   : dw I_write, "                                " ; 14
+!small(white)   : dw I_write, "                                " ; 15
+!small(pink)    : dw I_write, "        SPECIAL THANKS TO       "
+!small(white)   : dw I_write, "                                "
+!large1(white)  : dw I_write, "              OI27              "
+!large2(white)  : dw I_write, "              OI27              "
+!small(white)   : dw I_write, "                                "
+!small(cyan)    : dw I_write, "        FOR THE HEX TWEAK       "
+!small(cyan)    : dw I_write, "   THAT INSPIRED THIS ROMHACK   "
+!small(white)   : dw I_write, "                                "
+!large1(yellow) : dw I_write, "         100FD4 TO 6B           "
+!large2(yellow) : dw I_write, "         100FD4 TO 6B           "
+!small(white)   : dw I_write, "                                " ; 1
+!small(white)   : dw I_write, "                                " ; 2
+!small(white)   : dw I_write, "                                " ; 3
+!small(white)   : dw I_write, "                                " ; 4
+!small(white)   : dw I_write, "                                " ; 5
+!small(white)   : dw I_write, "                                " ; 6
+!small(white)   : dw I_write, "                                " ; 7
+!small(white)   : dw I_write, "                                " ; 8
+!small(white)   : dw I_write, "                                " ; 9
+!small(white)   : dw I_write, "                                " ; 10
+!small(white)   : dw I_write, "                                " ; 11
+!small(white)   : dw I_write, "                                " ; 12
+!small(white)   : dw I_write, "                                " ; 13
+!small(white)   : dw I_write, "                                " ; 14
+!small(white)   : dw I_write, "                                " ; 15
+!small(pink)    : dw I_write, "  THIS HACK WAS MADE WITH LOVE  "
+!small(pink)    : dw I_write, "   FOR MY SON AND MY DAUGHTER   "
+!small(white)   : dw I_write, "                                "
+!large1(white)  : dw I_write, "           E  AND  G            "
+!large2(white)  : dw I_write, "           E  AND  G            "
+.extra_credits_end:
 
   dw I_set_timer, !credits_end_lines
 - dw I_draw, blank_line
@@ -97,6 +138,11 @@ baby_metroid_credits:
 
   dw I_end_credits
   dw I_delete
+
+  !extra_credit_lines_used := (.extra_credits_end-.extra_credits_start)/68-1
+  print "Extra credit lines used: ", dec(!extra_credit_lines_used)
+
+  assert !extra_credit_lines_used = !extra_credit_lines
 }
 
 global end_credits_freespace_b8:
