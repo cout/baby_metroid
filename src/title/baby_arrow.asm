132e51096641392660efbb40eabdaa81a00513a4
Paul Brannan <cout@users.noreply.github.com>
2023-02-02 18:08:12 -0500

    Use the baby on the options menu

--- src/title/baby_arrow.asm
+++ src/title/baby_arrow.asm
@@ -1,6 +1,13 @@
-; TODO: 
-; - select the correct palette (though this looks pretty good)
-; - make a similar change on the options menu
+; TODO: select the correct palette (though this looks pretty good)
 
+; Use the baby for the main menu selection icon
 org $82BAB2
 dw $0065, $0066, $0067, $0066
+
+; Use the baby for the options menu selection icon
+org $82F442
+dw $0008, $CFF6
+dw $0008, $CFFD
+dw $0008, $D004
+dw $0008, $CFFD
+dw $8C82, $F442
